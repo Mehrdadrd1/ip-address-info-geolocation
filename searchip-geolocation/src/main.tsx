572df4cb39c93f1f0dac28b8c47ab54662320982
Mehrdad Rayati <rayati.mehrdad@gmail.com>
2024-10-13 21:14:19 +0330

    theme and global style is added

--- searchip-geolocation/src/main.tsx
+++ searchip-geolocation/src/main.tsx
@@ -1,10 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { ThemeProvider } from "styled-components";
+import { theme, GlobalStyle } from "./theme";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <App />
+    </ThemeProvider>
+  </StrictMode>
+);
---
+++ searchip-geolocation/src/theme/theme.ts
@@ -0,0 +1,16 @@
+export const theme = {
+  colors: {
+    background: "#FAFAFA",
+    GradientColor: "linear-gradient(45deg,#1043A6,#0C317C)",
+    commonWhite: "#FFFFFF",
+    border: "#EAECF0",
+    text: {
+      primary: "#313235",
+      secondary: "#F9F5FF",
+      disable: "#7E838F",
+    },
+    breakpoints: {
+      mobile: "600px",
+    },
+  },
+};
